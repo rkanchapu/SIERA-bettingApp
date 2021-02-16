330265d6c851fddafaa3253e69a545e5d4a0c6b7
Rajya Lakshmi Kanchapu <razik120291@gmail.com>
2021-02-16 14:39:12 -0500

    change of http to https

--- src/appConfig.js
+++ src/appConfig.js
@@ -1,5 +1,5 @@
 export default function appConfig() {
-  const MockAPI = "http://www.mocky.io/v2/59f08692310000b4130e9f71";
+  const MockAPI = "https://www.mocky.io/v2/59f08692310000b4130e9f71";
   const REACT_APP_NAME = "Siera - betting app"
 
   return {
